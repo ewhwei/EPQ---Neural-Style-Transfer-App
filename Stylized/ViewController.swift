d46c720e33a89f5e684e67c664787258447bd7d2
Imafont <0220161718@harrowschool.hk>
2022-07-11 16:28:57 +0800

    Initial Commit

---
+++ Stylized/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stylized
+//
+//  Created by Edward Wei on 11/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
